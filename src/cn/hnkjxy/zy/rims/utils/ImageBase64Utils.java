598e2a602b30bd1c68ed135af7f72d1eb708b10c
9527Number <1024830255@qq.com>
2018-11-14 23:33:42 +0800

    删除测试方法,更改使之符合项目,注释掉imageToBase64ByOnline方法

--- src/cn/hnkjxy/zy/rims/utils/ImageBase64Utils.java
+++ src/cn/hnkjxy/zy/rims/utils/ImageBase64Utils.java
@@ -1,6 +1,5 @@
 package cn.hnkjxy.zy.rims.utils;
 
-import org.junit.jupiter.api.Test;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
@@ -9,25 +8,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
+ * 图片Base64互转工具类
+ *
  * @author 10248
  */
 public class ImageBase64Utils {
-
-    @Test
-    public void testBase64Utils() throws Exception {
-
-        //本地图片地址
-        String url = "./img/dish_img/huncai1.png";
-        //在线图片地址
-//        String string = "http://bpic.588ku.com//element_origin_min_pic/17/03/03/7bf4480888f35addcf2ce942701c728a.jpg";
-
-        String str = ImageBase64Utils.imageToBase64ByLocal(url);
-
-        System.out.println(str);
-
-        ImageBase64Utils.base64ToImage(str, "./img/dish_img/test1.png");
-
-    }
+    final private static String imgPath = "./img/dish_img/";
 
     /**
      * 本地图片转换成base64字符串
@@ -35,10 +21,12 @@ public class ImageBase64Utils {
      * @param imgFile 图片本地路径
      * @return Base64字符串
      */
-    public static String imageToBase64ByLocal(String imgFile) {// 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+    public static String base64ImageReadByLocal(String imgFile) {// 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        InputStream in;
 
+        //文件名加上图片路径
+        imgFile += imgPath;
 
-        InputStream in = null;
         byte[] data = null;
 
         // 读取图片字节数组
@@ -58,39 +46,37 @@ public class ImageBase64Utils {
         return encoder.encode(data);
     }
 
-
-    /**
-     * 在线图片转换成base64字符串
-     *
-     * @param imgURL 图片线上路径
-     * @return Base64字符串
-     */
-    public static String imageToBase64ByOnline(String imgURL) {
-        ByteArrayOutputStream data = new ByteArrayOutputStream();
-        try {
-            // 创建URL
-            URL url = new URL(imgURL);
-            byte[] by = new byte[1024];
-            // 创建链接
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setConnectTimeout(5000);
-            InputStream is = conn.getInputStream();
-            // 将内容读取内存中
-            int len = -1;
-            while ((len = is.read(by)) != -1) {
-                data.write(by, 0, len);
-            }
-            // 关闭流
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // 对字节数组Base64编码
-        BASE64Encoder encoder = new BASE64Encoder();
-        return encoder.encode(data.toByteArray());
-    }
-
+//    /**
+//     * 在线图片转换成base64字符串
+//     *
+//     * @param imgURL 图片线上路径
+//     * @return Base64字符串
+//     */
+//    public static String imageToBase64ByOnline(String imgURL) {
+//        ByteArrayOutputStream data = new ByteArrayOutputStream();
+//        try {
+//            // 创建URL
+//            URL url = new URL(imgURL);
+//            byte[] by = new byte[1024];
+//            // 创建链接
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//            conn.setRequestMethod("GET");
+//            conn.setConnectTimeout(5000);
+//            InputStream is = conn.getInputStream();
+//            // 将内容读取内存中
+//            int len = -1;
+//            while ((len = is.read(by)) != -1) {
+//                data.write(by, 0, len);
+//            }
+//            // 关闭流
+//            is.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        // 对字节数组Base64编码
+//        BASE64Encoder encoder = new BASE64Encoder();
+//        return encoder.encode(data.toByteArray());
+//    }
 
     /**
      * base64字符串转换成图片
@@ -98,10 +84,13 @@ public class ImageBase64Utils {
      * @param imgStr      base64字符串
      * @param imgFilePath 图片存放路径
      */
-    public static boolean base64ToImage(String imgStr, String imgFilePath) { // 对字节数组字符串进行Base64解码并生成图片
+    public static boolean base64ImageSaveToLocal(String imgStr, String imgFilePath) { // 对字节数组字符串进行Base64解码并生成图片
+
+        //文件名加上图片路径
+        imgFilePath += imgPath;
 
         //判断图像数据为空
-        if (imgStr == null || imgStr.equals("")) {
+        if (imgStr == null || "".equals(imgStr)) {
             return false;
         }
 
