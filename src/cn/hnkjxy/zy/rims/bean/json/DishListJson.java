40721183167e94c3f109244b24ea0103ade9f601
Tang <1024830255@qq.com>
2018-11-28 16:53:51 +0800

    用于解析DishList的实体类

---
+++ src/cn/hnkjxy/zy/rims/bean/json/DishListJson.java
@@ -0,0 +1,18 @@
+package cn.hnkjxy.zy.rims.bean.json;
+
+import java.util.List;
+
+/**
+ * @author 10248
+ */
+public class DishListJson {
+    private List<Integer> dishList;
+
+    public List<Integer> getDishList() {
+        return dishList;
+    }
+
+    public void setDishList(List<Integer> dishList) {
+        this.dishList = dishList;
+    }
+}
